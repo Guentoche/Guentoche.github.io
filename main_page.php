c06c757b6995a91eecfc0d781ffb4b4029cc1c75
Guentoche <pean.theo@hotmail.fr>
2019-12-09 23:27:17 +0100

    second commit

---
+++ main_page.php
@@ -0,0 +1,39 @@
+<php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Portfolio</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body style="height:1500px">
+
+<nav class="navbar navbar-inverse ">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Mon site web</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Accueil</a></li>
+      <li><a href="#">A propos de moi</a></li>
+      <li><a href="#">Compétences</a></li>
+      <li><a href="#">Expériences</a></li>
+      <li><a href="">Formations</a></li>
+      <li><a href="">Réalisations</a></li>
+      <li><a href="">Contact</a></li>
+    </ul>
+  </div>
+</nav>
+  
+<div class="container" style="margin-top:50px">
+  <h3>Titre blabla</h3>
+  <p>Texte de blabla.</p>
+</div>
+
+</body>
+</html>
